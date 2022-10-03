a6214fd2d615fe14b5c64fe55e1010afcb543e69
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-10-03 09:14:25 +0700

    user book revise

--- src/pages/userbook/UserbookList.jsx
+++ src/pages/userbook/UserbookList.jsx
@@ -80,8 +80,9 @@ export default function UserBookList() {
                                     <td>{userBooks.returnDate}</td>
                                     <td>
                                         <Link to=
-                                                  // {"/userbook/form/" + userBooks.userBookId}
-                                                  {"/userbook/form" + JSON.stringify(userBooks)}>
+                                                   {"/userbook/form/" + userBooks.userBookId}
+                                                  //{"/userbook/form" + JSON.stringify(userBooks)}
+                                            >
                                             <button className="btn btn-primary"> Edit </button>
                                         </Link>{" "}
                                         <button
