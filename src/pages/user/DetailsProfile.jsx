a1f0bf164316ea9e709f2b166dcae123a5f74364
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-10-04 12:08:24 +0700

    all revise redeploy BE

--- src/pages/user/DetailsProfile.jsx
+++ src/pages/user/DetailsProfile.jsx
@@ -63,7 +63,7 @@ export default function DetailsProfile() {
             returnDates = new Date(userBooks[i].returnDate)
             let userBooksDetailsArr = []
             if(userBooks[i].userName === params.username ){
-                if(userBooks[i].returnDate !== null){
+                if(returnDates !== null){
                     //userBooksDetailsArr.pop()
                     totalBooks = totalBooks - 1
                     if(totalBooks < 0 ){
